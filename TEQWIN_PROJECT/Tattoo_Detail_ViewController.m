85018bfda69b3d07c8c69dde010ec3ea336b7094
lys2907 <teqwin@Teqwins-MacBook-Pro.local>
2014-11-14 11:28:36 +0800

    14-11-2014 11:28

--- TEQWIN_PROJECT/Tattoo_Detail_ViewController.m
+++ TEQWIN_PROJECT/Tattoo_Detail_ViewController.m
@@ -12,8 +12,8 @@
 #import "SWRevealViewController.h"
 #import "TattooMaster_ViewController.h"
 #import "Master_Map_ViewController.h"
-#import "FavDataManager.h"
-#import "Venue.h"
+
+
 #import "Map_ViewController.h"
 #import "LoginUIViewController.h"
 @import CoreData;
