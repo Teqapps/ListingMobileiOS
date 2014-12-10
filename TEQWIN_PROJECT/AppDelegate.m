f23f13594aad4de25b68803c45cfbd7f41d0fd7c
lys2907 <teqwin@Teqwins-MacBook-Pro.local>
2014-12-10 12:02:13 +0800

    10-12-2014 12:02

--- TEQWIN_PROJECT/AppDelegate.m
+++ TEQWIN_PROJECT/AppDelegate.m
@@ -35,7 +35,17 @@
     [PFFacebookUtils initializeFacebook];
         [PFTwitterUtils initializeWithConsumerKey:@"your_twitter_consumer_key" consumerSecret:@"your_twitter_consumer_secret"];
 
+    if ([[[UIDevice currentDevice] systemVersion] floatValue] >= 8.0)
+    {
+        [application registerUserNotificationSettings:[UIUserNotificationSettings settingsForTypes:(UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge) categories:nil]];
+        [application registerForRemoteNotifications];
+    }
+    else
+    {
+        [application registerForRemoteNotificationTypes:UIRemoteNotificationTypeAlert|UIRemoteNotificationTypeBadge|UIRemoteNotificationTypeSound];
+    }
 
+    
     [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
     PFACL *defaultACL = [PFACL ACL];
     [defaultACL setPublicReadAccess:YES];
